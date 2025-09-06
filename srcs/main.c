b83f86f7c449a88e242d60aeabefc34e9b6b131f
Lea Devoudelle-Royer <ldevoude@z1r7p4.42lyon.fr>
2025-09-06 13:42:49 +0200

    feature(main.c): did the pseudocode for our main

--- srcs/main.c
+++ srcs/main.c
@@ -2,6 +2,13 @@
 
 int main(void)
 {
-    write(1,"Hello, world\n", 13);
+    //parsing
+      //if parsing fail return error with right msg
+    //init structures/data
+        //if fail return error with right msg + free close
+    //init minilibx + I guess the controls? 
+        //if fail return error with right msg + free/close
+    //running part
+    //write(1,"Hello, world\n", 13);
     return (0);
 }
