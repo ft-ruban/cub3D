f6098bdca4056a15924a9d150599929af7fa0aad
Mathis Ballet <maballet@z1r6p2.42lyon.fr>
2025-10-02 15:47:53 +0200

    add(exec.h): we added a player strucure in this header

--- include/exec.h
+++ include/exec.h
@@ -4,6 +4,14 @@
 # include "cub3D.h"
 # include "set_mlx.h"
 
+typedef struct s_player_pos
+{
+	float pos_x;
+	float pos_y;
+	float delta;
+	
+} t_player_pos;
+
 //draw.c
 int	    draw(t_mlx *mlx);
 void	my_mlx_pixel_put(t_img *data, int x, int y, int color);
